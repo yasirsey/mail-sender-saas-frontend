6996fb462f33e4866275602740b39f6c5738a11d
yasirsey <yasirsey@gmail.com>
2025-07-17 18:24:49 +0300

    bug fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -22,7 +22,7 @@ import {
 
 // Create axios instance
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    baseURL: 'http://66.42.82.115/api',
     headers: {
         'Content-Type': 'application/json',
     },
